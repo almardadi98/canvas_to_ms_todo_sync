5553bd3bc54fc2893c15b384734423c97083ba85
Almar Daði Björnsson <almardadi@outlook.com>
2022-04-17 00:13:59 +0000

    MS TODO settings refactor

--- src/mstodo.py
+++ src/mstodo.py
@@ -3,26 +3,30 @@ from pymstodo import ToDoConnection
 from models.ms_todo_settings import MSTODOSettings
 import pickle
 
-with open("./conf/ms_todo_settings.json", "r", encoding="UTF-8") as mstodo_file:
-    mstodo_settings = MSTODOSettings(**json.load(mstodo_file))
 
-client_id = mstodo_settings.CLIENT_ID
-client_secret = mstodo_settings.CLIENT_SECRET
+def get_ms_todo_settings() -> MSTODOSettings:
+    with open("./conf/ms_todo_settings.json", "r", encoding="UTF-8") as mstodo_file:
+        mstodo_settings = MSTODOSettings(**json.load(mstodo_file))
+    return mstodo_settings
 
 
 def connect_to_ms_todo() -> ToDoConnection:
+    mstodo_settings = get_ms_todo_settings()
+    # Load the auth token from file
     try:
         with open("./data/token.dat", "rb") as file:
             token = pickle.load(file)
     except FileNotFoundError:
-        auth_url = ToDoConnection.get_auth_url(client_id)
+        # Token does not exist. Proceed to get a new token
+        auth_url = ToDoConnection.get_auth_url(mstodo_settings.CLIENT_ID)
         redirect_resp = input(
             f'Go here and authorize:\n{auth_url}\n\nPaste the full redirect URL below:\n')
         token = ToDoConnection.get_token(
-            client_id, client_secret, redirect_resp)
+            mstodo_settings.CLIENT_ID, mstodo_settings.CLIENT_SECRET, redirect_resp)
+        # Write the token to disk
         with open("./data/token.dat", "wb") as file:
             pickle.dump(token, file)
 
     todo_client = ToDoConnection(
-        client_id=client_id, client_secret=client_secret, token=token)
+        client_id=mstodo_settings.CLIENT_ID, client_secret=mstodo_settings.CLIENT_SECRET, token=token)
     return todo_client
